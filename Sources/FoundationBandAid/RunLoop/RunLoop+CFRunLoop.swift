163e98760ce824d9c880684ec1014228795ef8c3
Dr. Michael Lauer <mickey@vanille-media.de>
2025-03-07 13:55:31 +0100

    RunLoop+CFRunLoop: make it available on Apple platforms as well

--- Sources/FoundationBandAid/RunLoop/RunLoop+CFRunLoop.swift
+++ Sources/FoundationBandAid/RunLoop/RunLoop+CFRunLoop.swift
@@ -1,12 +1,14 @@
-#if compiler(>=6.0)
 import Foundation
 #if canImport(CoreFoundation)
 import CoreFoundation
 #endif
 
 extension RunLoop {
-    @inlinable
-    @inline(__always)
-    var CC_cfRunLoop: CFRunLoop { unsafeBitCast(self, to: CFRunLoop.self) }
-}
+#if canImport(ObjectiveC)
+    @inlinable @inline(__always)
+    public var CC_cfRunLoop: CFRunLoop { self.getCFRunLoop() }
+#else
+    @inlinable @inline(__always)
+    public var CC_cfRunLoop: CFRunLoop { unsafeBitCast(self, to: CFRunLoop.self) }
 #endif
+}
